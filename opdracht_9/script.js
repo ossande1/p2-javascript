1a86e924126071b89cb0121f7ffa344a848b5e0f
Ossande Gima Da silva <9030460@student.zadkine.nl>
2025-11-28 14:02:08 +0100

    Add files via upload

---
+++ opdracht_9/script.js
@@ -0,0 +1,7 @@
+function verberg_tekst(){
+   document.getElementById("op9").style.display = "none"
+}
+function  toon_tekst(){
+   document.getElementById("op9").style.display = "block"
+   document.getElementById("op9").style.backgroundColor = "lightyellow"
+}
