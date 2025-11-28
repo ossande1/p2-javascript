713588b434e838d32df296b95cace8eef7726a93
Ossande Gima Da silva <9030460@student.zadkine.nl>
2025-11-28 14:50:54 +0100

    Add files via upload

---
+++ opdracht_10/script.js
@@ -0,0 +1,17 @@
+function groet_ochtend(){
+    document.getElementById("op10").innerHTML = "Goedemorgen";
+    document.body.style.backgroundColor = "lightyellow"
+    document.body.style.color = "black"
+}
+
+function groet_middag(){
+    document.getElementById("op10").innerHTML = "Goedemiddag";
+    document.body.style.backgroundColor = "#FFD580"
+    document.body.style.color = "black"
+}
+
+function groet_avond(){
+    document.getElementById("op10").innerHTML = "Goedenavond";
+    document.body.style.backgroundColor = "darkblue"
+    document.body.style.color = "white"
+}
