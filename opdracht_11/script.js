c6a14862d3f253949e9c455efef3ed28275db4d4
Ossande Gima Da silva <9030460@student.zadkine.nl>
2025-12-05 10:18:27 +0100

    Add files via upload

---
+++ opdracht_11/script.js
@@ -0,0 +1,11 @@
+let budget  = 100;
+
+let product = 60;
+
+if (product <= budget) {
+  alert("U heeft genoeg geld!");
+  style.color = "green"
+} else {
+  alert("Helaas, te weinig geld");
+  syle.color = "red"
+}
