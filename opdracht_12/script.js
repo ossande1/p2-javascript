33c27c62241cc46d5fe1b286a67e50781630e734
Ossande Gima Da silva <9030460@student.zadkine.nl>
2025-12-08 12:59:44 +0100

    Add files via upload

---
+++ opdracht_12/script.js
@@ -0,0 +1,10 @@
+let product = prompt("Hoeveel kost het product dat je wilt kopen?");
+
+let budget  = 100;
+
+if (product <= budget) {
+  alert("U heeft genoeg geld!");
+} 
+else {
+  alert("Helaas, te weinig geld");
+}
