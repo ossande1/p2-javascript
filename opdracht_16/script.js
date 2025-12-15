4a7da1c9a1b01147d706efd72b1212d013f1b423
Ossande Gima Da silva <9030460@student.zadkine.nl>
2025-12-15 10:47:01 +0100

    Add files via upload

---
+++ opdracht_16/script.js
@@ -0,0 +1,8 @@
+let knop = document.getElementById("op16");
+
+knop.addEventListener("click", klik,);
+
+function klik() {
+  document.getElementById("op16").innerHTML = "Doeg"
+  document.getElementById("op16").style.color = "red"
+}
