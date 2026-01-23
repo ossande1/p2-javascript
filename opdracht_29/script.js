18a2636a7dd37071f44ce247a6905e1373caaf55
Ossande Gima Da silva <9030460@student.zadkine.nl>
2026-01-23 13:09:04 +0100

    Add files via upload

---
+++ opdracht_29/script.js
@@ -0,0 +1,40 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function pop(){
+    let age = document.getElementById("age").value;
+
+    if(age >= 18){
+        window.location.href = "https://www.google.com";
+    } else {
+        modal.style.display = "none";
+        document.getElementById("overlay").style.display = "block";
+    }
+}
